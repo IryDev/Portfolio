b86b31fc99d9ba3be7419735c08b7f8ffa974bc3
IryDev <86270481+IryDev@users.noreply.github.com>
2023-07-22 14:36:19 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,3 +16,27 @@ function volet() {
 hamburgerMenu.addEventListener("click", volet);
 contact_button.addEventListener("click", open);
 contact_button_mobile.addEventListener("click", open)
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("nav a");
+
+  // Fonction pour mettre à jour la classe active en fonction de la section visible
+  function updateActiveNavLink() {
+    const sections = document.querySelectorAll("section");
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+        const targetId = section.getAttribute("id");
+        navLinks.forEach((link) => {
+          link.classList.remove("active");
+          if (link.getAttribute("href") === `#${targetId}`) {
+            link.classList.add("active");
+          }
+        });
+      }
+    });
+  }
+
+  // Écouteur d'événement de défilement pour mettre à jour les liens actifs
+  window.addEventListener("scroll", updateActiveNavLink);
+});
